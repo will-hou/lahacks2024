5bc3251cca45aa011f7fc60d0d1dc2997ff3be16
William Hou <whou125@gmail.com>
2024-04-20 17:20:39 -0700

    Add tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </React.StrictMode>
 )

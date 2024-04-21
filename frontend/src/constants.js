e2b73dc5aab45bbf29c2a2a2c266699decaedc3a
William Hou <whou125@gmail.com>
2024-04-20 22:51:22 -0700

    Prod endpoint

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,5 @@
-export const BACKEND_ENDPOINT = 'http://localhost:8000'
+// export const BACKEND_ENDPOINT = 'http://localhost:8000'
+
+// Prod
+export const BACKEND_ENDPOINT = 'https://lahacks2024.onrender.com/'
 
